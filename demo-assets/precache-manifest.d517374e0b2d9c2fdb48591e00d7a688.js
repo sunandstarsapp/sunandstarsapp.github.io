1b6462305ee70fa8a75b6b759b2e662070f20130
Dan Chenier <daniel.chenier@edmonton.ca>
2019-01-15 06:30:44 -0700

    Initial commit for app demo

---
+++ demo-assets/precache-manifest.d517374e0b2d9c2fdb48591e00d7a688.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "6730475954aa2dc14e38",
+    "url": "/demo-assets/static/js/runtime~main.67304759.js"
+  },
+  {
+    "revision": "a9456dec9cc8abebc78e",
+    "url": "/demo-assets/static/js/main.a9456dec.chunk.js"
+  },
+  {
+    "revision": "3d32ded24b061e109af6",
+    "url": "/demo-assets/static/js/1.3d32ded2.chunk.js"
+  },
+  {
+    "revision": "c95990025325f4675112813bcfeb601d",
+    "url": "/demo-assets/index.html"
+  }
+];
